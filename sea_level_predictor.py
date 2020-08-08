1493fb8e157017569d033ade9f7d459b30c2eca5
Emely Diaz <45925710+azidmiracle@users.noreply.github.com>
2020-08-08 23:44:59 +0800

    Add files via upload

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -11,23 +11,28 @@ def draw_plot():
     y=df['CSIRO Adjusted Sea Level']
 
     # Create scatter plot
-    x_yrs_extended=np.arange(1850,2100,25)
-    plt.figure(figsize=(30,10))
+    # Create scatter plot
+    #x_yrs_extended=np.arange(1850,2050,25)
+    x_yrs_extended=np.arange(x.min(),2050)
+    plt.figure(figsize=(50,10))
     plt.scatter(x=x,y=y,marker='o')
     plt.xticks(range(1850,2100,25))
 
     # Create first line of best fit
+    
     slope,intercept,r_value,p_value,std_err=linregress(x,y)
     line = [slope*xi + intercept for xi in x_yrs_extended]
+    #line = slope*x + intercept 
     plt.plot(x_yrs_extended,line,'r',label='fitted line')
 
     # Create second line of best fit
     df_2020=df[df['Year']>=2000]
     x_2000=df_2020['Year']
     y_2000=df_2020['CSIRO Adjusted Sea Level']
+    x_yrs_extended_2=np.arange(x_2000.min(),2050)
     slope_2,intercept_2,r_value_2,p_value_2,std_err_2=linregress(x_2000,y_2000)
-    line_2 = [slope_2*xi + intercept_2 for xi in x_yrs_extended]
-    plt.plot(x_yrs_extended,line_2,'r',label='fitted line 2')
+    line_2 = [slope_2*xi + intercept_2 for xi in x_yrs_extended_2]
+    plt.plot(x_yrs_extended_2,line_2,'r',label='fitted line 2')
 
 
 # Add labels and title
